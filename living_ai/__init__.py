af9fc713e48d42e8cc7b4a9888a31c75a6241448
Charles Washington Juma <tnxsageofficiall@gmail.com>
2026-06-27 15:23:52 +0300

    Create      __init__.py

---
+++ living_ai/__init__.py
@@ -0,0 +1,17 @@
+"""
+Living AI Framework
+
+Persistent Adaptive Intelligence Framework
+
+Public API
+"""
+
+from core.system_engine import LivingAISystem
+
+__version__ = "0.1.0"
+
+__author__ = "Charles Washington Juma"
+
+__all__ = [
+    "LivingAISystem",
+]
